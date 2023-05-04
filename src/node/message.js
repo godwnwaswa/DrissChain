d8ff3fe1e22656215a7cb61b5624864c37103fb4
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-04 07:59:02 +0300

    documented

--- src/node/message.js
+++ src/node/message.js
@@ -1,10 +1,21 @@
+/**
+ * Returns a JSON stringified object with the given type and data.
+ *
+ * @param {string} type - The type of message.
+ * @param {any} data - The data to include in the message.
+ * @returns {string} - A JSON stringified object with the given type and data.
+ */
 function produceMessage(type, data) {
-    // Produce a JSON message
     return JSON.stringify({ type, data });
 }
 
+/**
+ * Sends the given message to each node in the given array of nodes.
+ *
+ * @param {string} message - The message to send.
+ * @param {Array} nodes - An array of nodes to send the message to.
+ */
 function sendMessage(message, nodes) {
-    // Broadcast message to all nodes
     nodes.forEach(node => node.socket.send(message));
 }
 
