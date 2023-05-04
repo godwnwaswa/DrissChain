58031794bcc84e1261709002eb7d3d3984337f2c
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-04 12:54:36 +0300

    documented

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,24 @@
 "use strict";
-
+/**
+ * Takes a number as input and returns the base 16 logarithm of that number.
+ * */
 function log16(x) {
     return Math.log(x) / Math.log(16);
 }
-
+/**
+ * Takes a string as input and returns a boolean indicating whether that string represents a number. 
+ * It checks each character in the string to see if it is a digit (0-9).
+ * 
+ * */
 function isNumber(str) {
     return str.split("").every(char => "0123456789".includes(char));
 }
 
+/**
+ * Takes a string as input and returns a boolean indicating whether that string can be converted 
+ * to a BigInt without throwing an error.
+ * 
+ * */
 function bigIntable(str) {
     try {
         BigInt(str);
@@ -18,6 +29,10 @@ function bigIntable(str) {
     }
 }
 
+/**
+ * Takes a string as input and attempts to parse it as JSON. If parsing succeeds, it returns the 
+ * parsed JSON object. If parsing fails, it returns an empty object.
+ * */
 function parseJSON(value) {
     let parsed;
     
@@ -30,6 +45,13 @@ function parseJSON(value) {
     return parsed;
 }
 
+/**
+ * Takes an array of transaction objects as input and returns an array of strings, where each string 
+ * represents a transaction object with its index in the input array appended to the beginning. 
+ * It does this by mapping over the input array and using JSON.stringify() to convert each transaction 
+ * object to a string.
+ * 
+ * */
 function indexTxns(transactions) {
     return transactions.map((txn, index) => index.toString() + JSON.stringify(txn));
 }
