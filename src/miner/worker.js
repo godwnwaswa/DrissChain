ad84c2ba85730b1de791aac290c3ca23e87b2404
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-09 11:40:17 +0300

    update, general

--- src/miner/worker.js
+++ src/miner/worker.js
@@ -1,48 +1,21 @@
-/**
- * This code defines a worker thread for a miner in a blockchain network.
- * */
-
 "use strict";
 
 const Block = require("../core/block");
 const { log16 } = require("../utils/utils");
 
 /**
- * 
- * -------------------------------------------------------------------------------------------------
- * The worker thread listens for messages from the main process and acts on the "MINE" message type. 
- * Upon receiving the message, it extracts the block and difficulty data from the message data.
- * -------------------------------------------------------------------------------------------------
- * 
- * -------------------------------------------------------------------------------------------------
- * The thread then starts a loop that incrementally increases the nonce value of the block and 
- * recalculates its hash until a hash with a specified number of leading zeros is obtained. 
- * The number of leading zeros required is calculated based on the difficulty parameter using 
- * the log16 function from the utils module.
- * -------------------------------------------------------------------------------------------------
- * 
- * -------------------------------------------------------------------------------------------------
- * Once a block with the required hash is found, the worker thread sends the result back to the main 
- * process using the send method of the process object.
- * -------------------------------------------------------------------------------------------------
- * 
- * 
+ * A worker thread for a miner in the Drisschain network.
  * */
 process.on("message", message => {
     if (message.type === "MINE") {
-        // When the "MINE" message is received, the thread should be mining by incrementing the nonce value until a preferable hash is met.
-
         const block = message.data[0];
         const difficulty = message.data[1];
 
         for (;;) {
-            // We will loop until the hash has "5+difficulty" starting zeros.
             if (block.hash.startsWith("00000" + Array(Math.floor(log16(difficulty)) + 1).join("0"))) {
                 process.send({ result: block });
-
                 break;
             }
-            
             block.nonce++;
             block.hash = Block.getHash(block);
         }
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,55 +1,41 @@
 "use strict";
 /**
- * Takes a number as input and returns the base 16 logarithm of that number.
+ * Calculates base 16 log of a number.
  * */
 function log16(x) {
     return Math.log(x) / Math.log(16);
 }
 /**
- * Takes a string as input and returns a boolean indicating whether that string represents a number. 
- * It checks each character in the string to see if it is a digit (0-9).
- * 
+ * Checks if a string represents a number; if each character is a digit (0-9).
  * */
 function isNumber(str) {
     return str.split("").every(char => "0123456789".includes(char));
 }
-
 /**
- * Takes a string as input and returns a boolean indicating whether that string can be converted 
- * to a BigInt without throwing an error.
- * 
+ * Checks if a string can be converted to a BigInt without throwing an error.
  * */
 function bigIntable(str) {
     try {
         BigInt(str);
-
         return true;
     } catch (e) {
         return false;
     }
 }
-
 /**
- * Takes a string as input and attempts to parse it as JSON. If parsing succeeds, it returns the 
- * parsed JSON object. If parsing fails, it returns an empty object.
+ * Parses as string as JSON object.
  * */
 function parseJSON(value) {
-    let parsed;
-    
     try {
-        parsed = JSON.parse(value);
+        return JSON.parse(value);
     } catch (e) {
         return {};
     }
-
-    return parsed;
 }
 
 /**
- * Takes an array of transaction objects as input and returns an array of strings, where each string 
- * represents a transaction object with its index in the input array appended to the beginning. 
- * It does this by mapping over the input array and using JSON.stringify() to convert each transaction 
- * object to a string.
+ * Converts an array of transaction objects to an array of string representation of 
+ * a transaction object & its index in the array appended to the beginning. 
  * 
  * */
 function indexTxns(transactions) {
