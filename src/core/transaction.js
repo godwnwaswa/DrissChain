35829c9f22a8fc089a22c62b25d75bfc0b98570e
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-11 05:24:28 +0300

    updated the tx class

--- src/core/transaction.js
+++ src/core/transaction.js
@@ -1,117 +1,104 @@
-"use strict"
+"use strict";
 
-const BN = require("bn.js") //arbitrary-precision integer arithmetic.
-const { isNumber } = require("../utils/utils")
-const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex")
-const EC = require("elliptic").ec, ec = new EC("secp256k1")
+const BN = require("bn.js");
+const { isNumber } = require("../utils/utils");
+const crypto = require("crypto");
+const { createHash } = crypto;
+const ec = new (require("elliptic").ec)("secp256k1");
+const { EMPTY_HASH } = require("../config.json");
 
-const { EMPTY_HASH } = require("../config.json")
+class Transaction {
+  constructor(
+    recipient = "",
+    amount = "0",
+    gas = "1000000000000",
+    additionalData = {},
+    nonce = 0
+  ) {
+    Object.assign(this, {
+      recipient,
+      amount,
+      gas,
+      additionalData,
+      nonce,
+      signature: {},
+    });
+  }
 
-class Transaction 
-{
-    constructor(recipient = "", amount = "0", gas = "1000000000000", additionalData = {}, nonce = 0) 
-    {
-        this.recipient      = recipient      // Recipient's address (public key)
-        this.amount         = amount         // Amount to be sent
-        this.gas            = gas            // Gas that transaction consumed + tip for miner
-        this.additionalData = additionalData // Additional data that goes into the transaction
-        this.nonce          = nonce           // Nonce for signature entropy
-        this.signature      = {}             // Transaction's signature, will be generated later
+  static getHash(tx) {
+    return createHash("sha256")
+      .update(
+        [
+          tx.recipient,
+          tx.amount,
+          tx.gas,
+          JSON.stringify(tx.additionalData),
+          tx.nonce.toString(),
+        ].join("")
+      )
+      .digest("hex");
+  }
+
+  static sign(transaction, keyPair) {
+    const sigObj = keyPair.sign(Transaction.getHash(transaction));
+    Object.assign(transaction.signature, {
+      v: sigObj.recoveryParam.toString(16),
+      r: sigObj.r.toString(16),
+      s: sigObj.s.toString(16),
+    });
+  }
+
+  static getPubKey(tx) {
+    const sigObj = {
+      r: new BN(tx.signature.r, 16),
+      s: new BN(tx.signature.s, 16),
+      recoveryParam: parseInt(tx.signature.v, 16),
+    };
+    const msgHash = Transaction.getHash(tx);
+    const txSenderPubkey = ec.recoverPubKey(
+      new BN(msgHash, 16).toString(10),
+      sigObj,
+      ec.getKeyRecoveryParam(msgHash, sigObj, ec.genKeyPair().getPublic())
+    );
+    return ec.keyFromPublic(txSenderPubkey).getPublic("hex");
+  }
+
+  static async isValid(tx, stateDB) {
+    const { recipient, amount, gas, additionalData, nonce } = tx;
+    const { contractGas } = additionalData;
+    if (
+      !(
+        typeof recipient === "string" &&
+        typeof amount === "string" &&
+        typeof gas === "string" &&
+        typeof additionalData === "object" &&
+        typeof nonce === "number" &&
+        (typeof contractGas === "undefined" ||
+          (typeof contractGas === "string" && isNumber(contractGas))) &&
+        isNumber(amount) &&
+        isNumber(gas)
+      )
+    ) {
+      return false;
     }
-
-    static getHash(tx) 
-    {
-        return SHA256
-        (
-            tx.recipient                      +
-            tx.amount                         +
-            tx.gas                            +
-            JSON.stringify(tx.additionalData) +
-            tx.nonce.toString()
-        )
+    const txSenderPubkey = Transaction.getPubKey(tx);
+    const txSenderAddress = createHash("sha256")
+      .update(txSenderPubkey)
+      .digest("hex");
+    if (!(await stateDB.keys().all()).includes(txSenderAddress)) {
+      return false;
     }
-
-    static sign(transaction, keyPair) 
-    {
-        const sigObj = keyPair.sign(Transaction.getHash(transaction))
-        transaction.signature = 
-        {
-            v: sigObj.recoveryParam.toString(16),
-            r: sigObj.r.toString(16),
-            s: sigObj.s.toString(16)
-        }
+    const { balance, codeHash } = await stateDB.get(txSenderAddress);
+    if (codeHash !== EMPTY_HASH) {
+      return false;
     }
-
-    static getPubKey(tx) 
-    {
-        // Get transaction's body's hash and recover original signature object
-        const msgHash = Transaction.getHash(tx)
-        const sigObj = {
-            r: new BN(tx.signature.r, 16),
-            s: new BN(tx.signature.s, 16),
-            recoveryParam: parseInt(tx.signature.v, 16)
-        }
-        // Recover public key and get real address.
-        const txSenderPubkey = ec.recoverPubKey
-        (
-            new BN(msgHash, 16).toString(10),
-            sigObj,
-            ec.getKeyRecoveryParam(msgHash, sigObj, ec.genKeyPair().getPublic())
-        )
-        return ec.keyFromPublic(txSenderPubkey).getPublic("hex")
-    }
-
-    /**
-     * ----------------------------------------------------------------------------------------------------------------------------
-     * The method first checks that the types of the properties of the `tx` object are valid, then tries to recover the public key 
-     * associated with the transaction's signature. If this fails, the method returns `false`. If the public key can be recovered, 
-     * the method computes the sender's address using the SHA256 hash of the sender's public key. If the state of the sender does 
-     * not exist, the method returns `false`. If the sender's address is associated with a contract, the method returns `false`. 
-     * Finally, the method checks that the sender has enough balance to pay for the transaction's cost and that the transaction's 
-     * amount is at least 0.
-     * -----------------------------------------------------------------------------------------------------------------------------
-     * */
-    static async isValid(tx, stateDB) 
-    {
-        if (
-            !(
-            typeof tx.recipient      === "string" &&
-            typeof tx.amount         === "string" &&
-            typeof tx.gas            === "string" &&
-            typeof tx.additionalData === "object" &&
-            typeof tx.nonce          === "number" &&
-            (
-                typeof tx.additionalData.contractGas === "undefined" ||
-                (
-                    typeof tx.additionalData.contractGas === "string" &&
-                    isNumber(tx.additionalData.contractGas)
-                )
-            ) &&
-            isNumber(tx.amount) &&
-            isNumber(tx.gas)
-        )) 
-        { return false }
-        let txSenderPubkey
-        try 
-        {
-            txSenderPubkey = Transaction.getPubKey(tx)
-        } 
-        catch (e) 
-        {
-            return false
-        }
-        const txSenderAddress = SHA256(txSenderPubkey)
-        if (!(await stateDB.keys().all()).includes(txSenderAddress)) return false
-        const dataFromSender = await stateDB.get(txSenderAddress)
-        const senderBalance = dataFromSender.balance
-        if (dataFromSender.codeHash !== EMPTY_HASH) return false
-        return (
-            (
-                BigInt(senderBalance) >= BigInt(tx.amount) + BigInt(tx.gas) + BigInt(tx.additionalData.contractGas || 0) && 
-                BigInt(tx.gas) >= 1000000000000n
-            ) &&  BigInt(tx.amount) >= 0 
-        )
-    }
+    return (
+      BigInt(balance) >=
+        BigInt(amount) + BigInt(gas) + BigInt(contractGas || 0) &&
+      BigInt(gas) >= 1000000000000n &&
+      BigInt(amount) >= 0
+    );
+  }
 }
 
-module.exports = Transaction
+module.exports = Transaction;
