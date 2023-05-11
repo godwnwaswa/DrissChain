17481414c1d1ed175ca76954785d799212015ad1
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-11 11:39:49 +0300

    removed semicolons

--- src/core/block.js
+++ src/core/block.js
@@ -7,7 +7,7 @@
  * using `this` in a function that is not a method of an object.
  * 
  * */
-"use strict";
+"use strict"
 
 /**
  * ----------------------------------------------------------------------------------
@@ -43,28 +43,28 @@
  * ----------------------------------------------------------------------
  * 
  * */
-const { Level } = require('level');
-const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex");
-const EC = require("elliptic").ec, ec = new EC("secp256k1");
-const Transaction = require("./transaction");
-const { buildMerkleTree } = require("./merkle");
-const { BLOCK_REWARD, BLOCK_GAS_LIMIT, EMPTY_HASH } = require("../config.json");
-const drisscript = require("./runtime");
-const { indexTxns } = require("../utils/utils");
+const { Level } = require('level')
+const crypto = require("crypto"), SHA256 = message => crypto.createHash("sha256").update(message).digest("hex")
+const EC = require("elliptic").ec, ec = new EC("secp256k1")
+const Transaction = require("./transaction")
+const { buildMerkleTree } = require("./merkle")
+const { BLOCK_REWARD, BLOCK_GAS_LIMIT, EMPTY_HASH } = require("../config.json")
+const drisscript = require("./runtime")
+const { indexTxns } = require("../utils/utils")
 
 class Block {
     constructor(blockNumber = 1, timestamp = Date.now(), transactions = [], difficulty = 1, parentHash = "", coinbase = "") {
-        this.transactions = transactions;                      // Transaction list
+        this.transactions = transactions                      // Transaction list
 
         // Block header
-        this.blockNumber  = blockNumber;                       // Block's index in chain
-        this.timestamp    = timestamp;                         // Block creation timestamp
-        this.difficulty   = difficulty;                        // Difficulty to mine block
-        this.parentHash   = parentHash;                        // Parent (previous) block's hash
-        this.nonce        = 0;                                 // Nonce
-        this.txRoot       = buildMerkleTree(indexTxns(transactions)).val; // Merkle root of transactions
-        this.coinbase     = coinbase;                          // Address to receive reward
-        this.hash         = Block.getHash(this);               // Hash of the block
+        this.blockNumber  = blockNumber                       // Block's index in chain
+        this.timestamp    = timestamp                         // Block creation timestamp
+        this.difficulty   = difficulty                        // Difficulty to mine block
+        this.parentHash   = parentHash                        // Parent (previous) block's hash
+        this.nonce        = 0                                 // Nonce
+        this.txRoot       = buildMerkleTree(indexTxns(transactions)).val // Merkle root of transactions
+        this.coinbase     = coinbase                          // Address to receive reward
+        this.hash         = Block.getHash(this)               // Hash of the block
     }
 
     /**
@@ -80,7 +80,7 @@ class Block {
             block.difficulty.toString()        +
             block.parentHash                   +
             block.nonce.toString()
-        );
+        )
     }
     /**
      * Checks if a given block instance has valid property types.
@@ -122,37 +122,37 @@ class Block {
      * */
     static async verifyTxAndTransit(block, stateDB, codeDB, enableLogging = false) {
         for (const tx of block.transactions) {
-            if (!(await Transaction.isValid(tx, stateDB))) return false;
+            if (!(await Transaction.isValid(tx, stateDB))) return false
         }
 
         // Get all existing addresses
-        const addressesInBlock = block.transactions.map(tx => SHA256(Transaction.getPubKey(tx)));
-        const existedAddresses = await stateDB.keys().all();
+        const addressesInBlock = block.transactions.map(tx => SHA256(Transaction.getPubKey(tx)))
+        const existedAddresses = await stateDB.keys().all()
 
         // If senders' address doesn't exist, return false
-        if (!addressesInBlock.every(address => existedAddresses.includes(address))) return false;
+        if (!addressesInBlock.every(address => existedAddresses.includes(address))) return false
         
         // Start state replay to check if transactions are legit
-        let states = {}, code = {}, storage = {};
+        let states = {}, code = {}, storage = {}
 
         for (const tx of block.transactions) {
-            const txSenderPubkey = Transaction.getPubKey(tx);
-            const txSenderAddress = SHA256(txSenderPubkey);
+            const txSenderPubkey = Transaction.getPubKey(tx)
+            const txSenderAddress = SHA256(txSenderPubkey)
             
             if (!states[txSenderAddress]) {
-                const senderState = await stateDB.get(txSenderAddress);
+                const senderState = await stateDB.get(txSenderAddress)
 
-                states[txSenderAddress] = senderState;
+                states[txSenderAddress] = senderState
                 
-                code[senderState.codeHash] = await codeDB.get(senderState.codeHash);
+                code[senderState.codeHash] = await codeDB.get(senderState.codeHash)
 
-                if (senderState.codeHash !== EMPTY_HASH) return false;
+                if (senderState.codeHash !== EMPTY_HASH) return false
  
-                states[txSenderAddress].balance = (BigInt(senderState.balance) - BigInt(tx.amount) - BigInt(tx.gas) - BigInt(tx.additionalData.contractGas || 0)).toString();
+                states[txSenderAddress].balance = (BigInt(senderState.balance) - BigInt(tx.amount) - BigInt(tx.gas) - BigInt(tx.additionalData.contractGas || 0)).toString()
             } else {
-                if (states[txSenderAddress].codeHash !== EMPTY_HASH) return false;
+                if (states[txSenderAddress].codeHash !== EMPTY_HASH) return false
 
-                states[txSenderAddress].balance = (BigInt(states[txSenderAddress].balance) - BigInt(tx.amount) - BigInt(tx.gas) - BigInt(tx.additionalData.contractGas || 0)).toString();
+                states[txSenderAddress].balance = (BigInt(states[txSenderAddress].balance) - BigInt(tx.amount) - BigInt(tx.gas) - BigInt(tx.additionalData.contractGas || 0)).toString()
             }
 
             // Contract deployment
@@ -160,38 +160,38 @@ class Block {
                 states[txSenderAddress].codeHash === EMPTY_HASH &&
                 typeof tx.additionalData.scBody === "string"
             ) {
-                states[txSenderAddress].codeHash = SHA256(tx.additionalData.scBody);
-                code[states[txSenderAddress].codeHash] = tx.additionalData.scBody;
+                states[txSenderAddress].codeHash = SHA256(tx.additionalData.scBody)
+                code[states[txSenderAddress].codeHash] = tx.additionalData.scBody
             }
 
             // Update nonce
-            states[txSenderAddress].nonce += 1;
+            states[txSenderAddress].nonce += 1
 
-            if (BigInt(states[txSenderAddress].balance) < 0n) return false;
+            if (BigInt(states[txSenderAddress].balance) < 0n) return false
 
             if (!existedAddresses.includes(tx.recipient) && !states[tx.recipient]) {
                 states[tx.recipient] = { balance: "0", codeHash: EMPTY_HASH, nonce: 0, storageRoot: EMPTY_HASH }
-                code[EMPTY_HASH] = "";
+                code[EMPTY_HASH] = ""
             }
         
             if (existedAddresses.includes(tx.recipient) && !states[tx.recipient]) {
-                states[tx.recipient] = await stateDB.get(tx.recipient);
-                code[states[tx.recipient].codeHash] = await codeDB.get(states[tx.recipient].codeHash);
+                states[tx.recipient] = await stateDB.get(tx.recipient)
+                code[states[tx.recipient].codeHash] = await codeDB.get(states[tx.recipient].codeHash)
             }
         
-            states[tx.recipient].balance = (BigInt(states[tx.recipient].balance) + BigInt(tx.amount)).toString();
+            states[tx.recipient].balance = (BigInt(states[tx.recipient].balance) + BigInt(tx.amount)).toString()
         
             // Contract execution
             if (states[tx.recipient].codeHash !== EMPTY_HASH) {
-                const contractInfo = { address: tx.recipient };
+                const contractInfo = { address: tx.recipient }
                 
-                const [ newState, newStorage ] = await drisscript(code[states[tx.recipient].codeHash], states, BigInt(tx.additionalData.contractGas || 0), stateDB, block, tx, contractInfo, enableLogging);
+                const [ newState, newStorage ] = await drisscript(code[states[tx.recipient].codeHash], states, BigInt(tx.additionalData.contractGas || 0), stateDB, block, tx, contractInfo, enableLogging)
         
                 for (const account of Object.keys(newState)) {
-                    states[account] = newState[account];
+                    states[account] = newState[account]
                 }
 
-                storage[tx.recipient] = newStorage;
+                storage[tx.recipient] = newStorage
             }
         }
 
@@ -199,74 +199,74 @@ class Block {
 
         if (!existedAddresses.includes(block.coinbase) && !states[block.coinbase]) {
             states[block.coinbase] = { balance: "0", codeHash: EMPTY_HASH, nonce: 0, storageRoot: EMPTY_HASH }
-            code[EMPTY_HASH] = "";
+            code[EMPTY_HASH] = ""
         }
     
         if (existedAddresses.includes(block.coinbase) && !states[block.coinbase]) {
-            states[block.coinbase] = await stateDB.get(block.coinbase);
-            code[states[block.coinbase].codeHash] = await codeDB.get(states[block.coinbase].codeHash);
+            states[block.coinbase] = await stateDB.get(block.coinbase)
+            code[states[block.coinbase].codeHash] = await codeDB.get(states[block.coinbase].codeHash)
         }
 
-        let gas = 0n;
+        let gas = 0n
 
         for (const tx of block.transactions) { gas += BigInt(tx.gas) + BigInt(tx.additionalData.contractGas || 0) }
 
-        states[block.coinbase].balance = (BigInt(states[block.coinbase].balance) + BigInt(BLOCK_REWARD) + gas).toString();
+        states[block.coinbase].balance = (BigInt(states[block.coinbase].balance) + BigInt(BLOCK_REWARD) + gas).toString()
 
         // Finalize state and contract storage into DB
 
         for (const address in storage) {
-            const storageDB = new Level(__dirname + "/../log/accountStore/" + address);
-            const keys = Object.keys(storage[address]);
+            const storageDB = new Level(__dirname + "/../log/accountStore/" + address)
+            const keys = Object.keys(storage[address])
 
-            states[address].storageRoot = buildMerkleTree(keys.map(key => key + " " + storage[address][key])).val;
+            states[address].storageRoot = buildMerkleTree(keys.map(key => key + " " + storage[address][key])).val
 
             for (const key of keys) {
-                await storageDB.put(key, storage[address][key]);
+                await storageDB.put(key, storage[address][key])
             }
 
-            await storageDB.close();
+            await storageDB.close()
         }
 
         for (const account of Object.keys(states)) {
-            await stateDB.put(account, states[account]);
+            await stateDB.put(account, states[account])
 
-            await codeDB.put(states[account].codeHash, code[states[account].codeHash]);
+            await codeDB.put(states[account].codeHash, code[states[account].codeHash])
         }
 
-        return true;
+        return true
     }
 
     static async hasValidTxOrder(block, stateDB) {
-        const nonces = {};
+        const nonces = {}
         
         for (const tx of block.transactions) {
-            const txSenderPubkey = Transaction.getPubKey(tx);
-            const txSenderAddress = SHA256(txSenderPubkey);
+            const txSenderPubkey = Transaction.getPubKey(tx)
+            const txSenderAddress = SHA256(txSenderPubkey)
 
             if (typeof nonces[txSenderAddress] === "undefined") {
-                const senderState = await stateDB.get(txSenderAddress);
+                const senderState = await stateDB.get(txSenderAddress)
 
-                nonces[txSenderAddress] = senderState.nonce;
+                nonces[txSenderAddress] = senderState.nonce
             }
 
-            if (nonces[txSenderAddress] + 1 !== tx.nonce) return false;
+            if (nonces[txSenderAddress] + 1 !== tx.nonce) return false
 
-            nonces[txSenderAddress]++;
+            nonces[txSenderAddress]++
         }
 
-        return true;
+        return true
     }
 
     static hasValidGasLimit(block) {
-        let totalGas = 0n;
+        let totalGas = 0n
 
         for (const tx of block.transactions) {
-            totalGas += BigInt(tx.additionalData.contractGas || 0);
+            totalGas += BigInt(tx.additionalData.contractGas || 0)
         }
 
-        return totalGas <= BigInt(BLOCK_GAS_LIMIT);
+        return totalGas <= BigInt(BLOCK_GAS_LIMIT)
     }
 }
 
-module.exports = Block;
+module.exports = Block
