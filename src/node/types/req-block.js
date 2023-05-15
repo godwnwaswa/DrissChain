0df5d9b154baeddbec36025a4a55aa3651474af3
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-15 14:45:11 +0300

    updated req-block

--- src/node/types/req-block.js
+++ src/node/types/req-block.js
@@ -1,13 +1,28 @@
-export const requestBlock = async (msg) => {
-    if (!ENABLE_CHAIN_REQUEST) {
-        const { blockNumber, requestAddress } = msg.data
-        const socket = opened.find(node => node.address === requestAddress).socket
-        const currentBlockNumber = Math.max(...(await blockDB.keys().all()).map(key => parseInt(key)))
-        if (blockNumber > 0 && blockNumber <= currentBlockNumber) {
-            const block = await blockDB.get(blockNumber.toString())
-            socket.send(produceMsg(TYPE.SEND_BLOCK, block))
-            fastify.log.info(`SEND_BLOCK* at height #${blockNumber} to ${requestAddress}.`)
-        }
+const pino = require('pino')
+const logger = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            ignore: 'pid,hostname',
+        },
+    },
+})
+const fastify = require('fastify')({
+    logger: logger
+})
+
+const TYPE = require("../message-types")
+const { produceMsg } = require("../message")
+
+export const requestBlock = async (msg, opened, blockDB) => {
+    const { blockNumber, requestAddress } = msg.data
+    const socket = opened.find(node => node.address === requestAddress).socket
+    const currentBlockNumber = Math.max(...(await blockDB.keys().all()).map(key => parseInt(key)))
+    if (blockNumber > 0 && blockNumber <= currentBlockNumber) {
+        const block = await blockDB.get(blockNumber.toString())
+        socket.send(produceMsg(TYPE.SEND_BLOCK, block))
+        fastify.log.info(`SEND_BLOCK* at height #${blockNumber} to ${requestAddress}.`)
     }
+
 
 }
