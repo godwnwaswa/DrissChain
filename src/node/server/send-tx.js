2892fa5299a88940f3f7f6336345087b571d5ed9
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-15 12:32:59 +0300

    updated sendtx

--- src/node/server/send-tx.js
+++ src/node/server/send-tx.js
@@ -1,7 +1,23 @@
+const pino = require('pino')
+const logger = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            ignore: 'pid,hostname',
+        },
+    },
+})
+const fastify = require('fastify')({
+    logger: logger
+})
+
+const { produceMsg, sendMsg } = require("../message")
+const TYPE = require("../message-types")
+
 /**
  * Broadcasts a transaction to other nodes.
 */
-const sendTx = async tx => {
+export const sendTx = async (tx, opened, chainInfo, stateDB) => {
     fastify.log.info("Tx received on Drisseum.")
     sendMsg(produceMsg(TYPE.CREATE_TRANSACTION, tx), opened)
     const res = await addTx(tx, chainInfo, stateDB)
