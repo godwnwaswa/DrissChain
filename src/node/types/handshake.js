a8c9e4e1dec3f2f3d638c690a1d1959e81d221ed
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-15 14:45:22 +0300

    updated

--- src/node/types/handshake.js
+++ src/node/types/handshake.js
@@ -1,7 +1,22 @@
-export const handshake = (msg) => {
+const pino = require('pino')
+const logger = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            ignore: 'pid,hostname',
+        },
+    },
+})
+const fastify = require('fastify')({
+    logger: logger
+})
+
+const {connect} = require('../server/connect')
+
+export const handshake = (msg, MAX_PEERS, MY_ADDRESS, address, connected, opened, connectedNodes) => {
     const address = msg.data
     if (connectedNodes <= MAX_PEERS) {
-        connect(MY_ADDRESS, address)
+        connect(MY_ADDRESS, address, connected, opened, connectedNodes)
     }
 
 }
