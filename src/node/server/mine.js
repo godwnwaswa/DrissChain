226b850b79a95c52c29cb98a90ee0f771d8cf062
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-15 11:54:10 +0300

    added dependent func

--- src/node/server/mine.js
+++ src/node/server/mine.js
@@ -123,3 +123,16 @@ const mine = async (publicKey, ENABLE_LOGGING) => {
         })
         .catch(err => fastify.log.error(err))
 }
+
+const loopMine = (publicKey, ENABLE_CHAIN_REQUEST, ENABLE_LOGGING, BLOCK_TIME) => {
+    let length = chainInfo.latestBlock.blockNumber
+    let mining = true
+
+    setInterval(async () => {
+        if (mining || length !== chainInfo.latestBlock.blockNumber) {
+            mining = false
+            length = chainInfo.latestBlock.blockNumber
+            if (!ENABLE_CHAIN_REQUEST) await mine(publicKey, ENABLE_LOGGING)
+        }
+    }, BLOCK_TIME)
+}
