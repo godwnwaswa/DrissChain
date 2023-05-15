db2d83fa847ceb37bea7763a1754d418eb4b1ef5
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-15 11:40:04 +0300

    isolated the connect func

--- src/node/server.js
+++ src/node/server.js
@@ -148,38 +148,7 @@ const startServer = async options => {
     }
     
 }
-/**
- * Connects to a WS server at the specified address.
- * */
-const connect = (MY_ADDRESS, address) => {
-    /**
-     * Check if the `address` is not already in the `connected` array and if it is not equal to `MY_ADDRESS`.
-     * */
-    if (!connected.find(peerAddress => peerAddress === address) && address !== MY_ADDRESS) {
-        const socket = new WS(address)
-        /**
-         * Open a connection to the socket and send a handshake message to all connected nodes.
-         * */
-        socket.on("open", async () => {
-            for (const _address of [MY_ADDRESS, ...connected]) socket.send(produceMsg(TYPE.HANDSHAKE, _address))
-            for (const node of opened) node.socket.send(produceMsg(TYPE.HANDSHAKE, address))
 
-            if (!opened.find(peer => peer.address === address) && address !== MY_ADDRESS) {
-                opened.push({ socket, address })
-            }
-            if (!connected.find(peerAddress => peerAddress === address) && address !== MY_ADDRESS) {
-                connected.push(address)
-                connectedNodes++
-                fastify.log.info(`Connected to ${address}.`)
-                socket.on("close", () => {
-                    opened.splice(connected.indexOf(address), 1)
-                    fastify.log.info(`Disconnected from ${address}.`)
-                })
-            }
-        })
-    }
-    return true
-}
 /**
  * Broadcasts a transaction to other nodes.
 */
