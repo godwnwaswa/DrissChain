5f239173f3236af9b515f24d30af38a83c6d15a6
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-15 15:55:04 +0300

    update

--- src/node/server/loop-mine.js
+++ src/node/server/loop-mine.js
@@ -1,14 +1,35 @@
-const { BLOCK_TIME } = require("../../config.json")
+const pino = require('pino')
+const logger = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            ignore: 'pid,hostname',
+        },
+    },
+})
+const fastify = require('fastify')({
+    logger: logger
+})
 
-export const loopMine = (publicKey, chainInfo, stateDB, BLOCK_GAS_LIMIT, ENABLE_CHAIN_REQUEST, ENABLE_LOGGING) => {
+const { BLOCK_TIME } = require("../../config.json")
+const {mine} = require('./mine')
+
+export const loopMine = (
+    publicKey, chainInfo, BLOCK_GAS_LIMIT, 
+    ENABLE_CHAIN_REQUEST, worker, stateDB, 
+    blockDB, bhashDB, codeDB, chainInfo, 
+    worker, mined) => {
+
     let length = chainInfo.latestBlock.blockNumber
     let mining = true
-
     setInterval(async () => {
         if (mining || length !== chainInfo.latestBlock.blockNumber) {
             mining = false
             length = chainInfo.latestBlock.blockNumber
-            if (!ENABLE_CHAIN_REQUEST) await mine(publicKey, BLOCK_GAS_LIMIT, stateDB, chainInfo, worker)
+            if (!ENABLE_CHAIN_REQUEST) await mine(
+                publicKey, BLOCK_GAS_LIMIT, stateDB, 
+                blockDB, bhashDB, codeDB, chainInfo, 
+                worker, mined)
         }
     }, BLOCK_TIME)
 }
