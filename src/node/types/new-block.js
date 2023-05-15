86aaaf50f8b6d63ea251a904123eb51bdadc01a1
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-15 15:02:16 +0300

    updated

--- src/node/types/new-block.js
+++ src/node/types/new-block.js
@@ -1,24 +1,46 @@
-export const newBlock = async (msg) => {
+const pino = require('pino')
+const logger = pino({
+    transport: {
+        target: 'pino-pretty',
+        options: {
+            ignore: 'pid,hostname',
+        },
+    },
+})
+const fastify = require('fastify')({
+    logger: logger
+})
+
+
+const { verifyBlock, updateDifficulty } = require("../../consensus/consensus")
+const { clearDepreciatedTxns } = require("../../core/txPool")
+const { sendMsg } = require("../message")
+
+export const newBlock = async (
+    msg, chainInfo, currentSyncBlock, 
+    stateDB, codeDB, blockDB, bhashDB, 
+    ENABLE_LOGGING, ENABLE_CHAIN_REQUEST, 
+    ENABLE_MINING, mined, worker) => {
+        
     const newBlock = msg.data
     if (!chainInfo.checkedBlock[newBlock.hash]) {
         chainInfo.checkedBlock[newBlock.hash] = true
     }
-    else {
-        return
-    }
+    else { return }
 
     if (!ENABLE_MINING){
         fastify.log.info("NEW_BLOCK* from peer. Verifying...")
     }
     
-    if (newBlock.parentHash !== chainInfo.latestBlock.parentHash && (!ENABLE_CHAIN_REQUEST || (ENABLE_CHAIN_REQUEST && currentSyncBlock > 1))) {
+    if (newBlock.parentHash !== chainInfo.latestBlock.parentHash && 
+        (!ENABLE_CHAIN_REQUEST || (ENABLE_CHAIN_REQUEST && currentSyncBlock > 1))) {
         chainInfo.checkedBlock[newBlock.hash] = true
         if (await verifyBlock(newBlock, chainInfo, stateDB, codeDB, ENABLE_LOGGING)) {
             fastify.log.info("Block verified. Syncing to the chain...")
             if (ENABLE_MINING) {
                 mined = true //check their chain length & sync if > your chain else mine
                 worker.kill()
-                worker = fork(`${__dirname}/../miner/worker.js`)
+                worker = fork(`${__dirname}/../../miner/worker.js`)
             }
             await updateDifficulty(newBlock, chainInfo, blockDB)
             await blockDB.put(newBlock.blockNumber.toString(), newBlock)
