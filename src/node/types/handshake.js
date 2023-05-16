fd31ba0e96a633cb48200f0a754778c3a364bb8e
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-16 16:35:23 +0300

    fix

--- src/node/types/handshake.js
+++ src/node/types/handshake.js
@@ -1,11 +1,9 @@
 const connect = require('../server/connect')
 
-const handshake = (
-    msg, MAX_PEERS, MY_ADDRESS, connected, opened, 
-    connectedNodes, fastify) => {
+const handshake = (msg, MAX_PEERS, MY_ADDRESS, connected, opened, connectedNodes, fastify) => {
     const address = msg.data
     if (connectedNodes <= MAX_PEERS) {
-        connect(MY_ADDRESS, address, connected, opened, connectedNodes, fastify)
+        return connect(MY_ADDRESS, address, connected, opened, connectedNodes, fastify)
     }
 
 }
