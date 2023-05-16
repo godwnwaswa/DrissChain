c26d51451e5a910e1cdc064a9eae39118faa0f09
Godwin Waswa <godwnwaswa@gmail.com>
2023-05-16 16:36:05 +0300

    added res obj

--- src/node/types/send-block.js
+++ src/node/types/send-block.js
@@ -3,33 +3,33 @@ const { verifyBlock, updateDifficulty } = require("../../consensus/consensus")
 const changeState = require("../../core/state")
 const TYPE = require("../message-types")
 
-const sendBlock = async (
-    msg, currentSyncBlock, chainInfo, 
-    stateDB, codeDB, blockDB, bhashDB, 
-    opened, MY_ADDRESS, ENABLE_CHAIN_REQUEST, fastify) => {
-    const block = msg.data
-    if (ENABLE_CHAIN_REQUEST && currentSyncBlock === block.blockNumber) {
+const sendBlock = async (msg, currentSyncBlock, chainInfo, stateDB, codeDB, blockDB,
+    bhashDB, opened, MY_ADDRESS, ENABLE_CHAIN_REQUEST, fastify) => {
+    const B = msg.data
+    const res = { opened, currentSyncBlock }
+    if (ENABLE_CHAIN_REQUEST && currentSyncBlock === B.blockNumber) {
+
         fastify.log.info("REQUEST_BLOCK* from peer. Verifying...")
-        if (chainInfo.latestSyncBlock === null || await verifyBlock(block, chainInfo, stateDB, codeDB, ENABLE_LOGGING)) {
+        if (chainInfo.latestSyncBlock === null || await verifyBlock(B, chainInfo, stateDB, codeDB, ENABLE_LOGGING)) {
             fastify.log.info("Block verified. Syncing to the chain...")
             currentSyncBlock += 1
-            await blockDB.put(block.blockNumber.toString(), block)
-            await bhashDB.put(block.hash, block.blockNumber.toString())
+            await blockDB.put(B.blockNumber.toString(), B)
+            await bhashDB.put(B.hash, B.blockNumber.toString())
             if (!chainInfo.latestSyncBlock) {
-                chainInfo.latestSyncBlock = block
-                await changeState(block, stateDB, codeDB)
+                chainInfo.latestSyncBlock = B
+                await changeState(B, stateDB, codeDB)
             }
-            chainInfo.latestBlock = block
-            await updateDifficulty(block, chainInfo, blockDB)
-            fastify.log.info(`Synced at height #${block.blockNumber}, chain state transited.`)
+            chainInfo.latestBlock = B
+            await updateDifficulty(B, chainInfo, blockDB)
+            fastify.log.info(`Synced at height #${B.blockNumber}, chain state transited.`)
 
             for (const node of opened) {
-                node.socket.send(prodMsg(TYPE.REQUEST_BLOCK,{ blockNumber: currentSyncBlock, requestAddress: MY_ADDRESS }))
+                node.socket.send(prodMsg(TYPE.REQUEST_BLOCK, { blockNumber: currentSyncBlock, requestAddress: MY_ADDRESS }))
                 await new Promise(r => setTimeout(r, 5000))
             }
         }
     }
-    
+    return res
 }
 
 module.exports = sendBlock
